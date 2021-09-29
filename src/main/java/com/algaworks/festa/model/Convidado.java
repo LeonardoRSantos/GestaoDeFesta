42c01aa84c28fd70c87681484c74f897459fde35
LeonardoRSantos <leonardoworkbench@gmail.com>
2021-09-29 16:33:35 -0300

    Gestão de Festa Final

---
+++ src/main/java/com/algaworks/festa/model/Convidado.java
@@ -0,0 +1,69 @@
+package com.algaworks.festa.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+public class Convidado implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    // H2
+    //@GeneratedValue(strategy = IDENTITY)
+    // PostgreSQL
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "convidado_generator")
+    @SequenceGenerator(name="convidado_generator", sequenceName = "convidado_seq", initialValue = 4)
+    private Long id;
+
+    private String nome;
+
+    private Integer quantidadeAcompanhantes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getQuantidadeAcompanhantes() {
+        return quantidadeAcompanhantes;
+    }
+
+    public void setQuantidadeAcompanhantes(Integer quantidadeAcompanhantes) {
+        this.quantidadeAcompanhantes = quantidadeAcompanhantes;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Convidado convidado = (Convidado) o;
+        return id.equals(convidado.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Convidado{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", quantidadeAcompanhantes=" + quantidadeAcompanhantes +
+                '}';
+    }
+}
---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+insert into convidado (id, nome, quantidade_acompanhantes) values (1, 'Leonardo', 2);
+insert into convidado (id, nome, quantidade_acompanhantes) values (2, 'Maria', 3);
+insert into convidado (id, nome, quantidade_acompanhantes) values (3, 'Ricardo', 1);
